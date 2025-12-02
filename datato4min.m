c3fea91d89b604330fc52964ca10c1c5d65e87f8
k0viub <117206239+k0viub@users.noreply.github.com>
2025-12-02 18:31:41 +0100

    Update datato4min.m

--- datato4min.m
+++ datato4min.m
@@ -1,5 +1,5 @@
 %read file
-data = load('/Volumes/kovi ssd/egyetem/7.felev-szakdoga/THUMB/patients/pt5_oiti37/awake/thumb2/oiti37_5_5_awake_thumb_2.mat');      
+data = load('/Volumes/kovi ssd/egyetem/7.felev-szakdoga/THUMB/patients/pt5/awake/thumb2/pt5_5_5_awake_thumb_2.mat');      
 lfpdataread = data.lfpdata;
 %%
 
@@ -16,17 +16,17 @@ lfpdataread([14 15], :) = 0; %will alter results
 
 % trim to 4 minutes
 fs = 2000;                                  % 2000 Hz for Pt1,5,6, 20000 for others
-dur_min = 4;                                 % minutes
+dur_min = 4;                                 % desired length in minutes
 n_keep = floor(dur_min * 60 * fs); 
 
 n_keep = min(n_keep, size(lfpdataread, 2));  % safety check
 
 %480000 or 4800000 long snippets
-lfpdata = lfpdataread(:, 1:n_keep); %first4
+lfpdata = lfpdataread(:, 1:n_keep); %first4mins
 %lfpdata = lfpdataread(:, 2*n_keep+1:3*n_keep); %from 8-12mins
 %%
 %save new data
-save('oiti37_5_5_awake_th2_4mins1.mat', 'lfpdata', '-v7.3');
+save('pt5__5_5_awake_th2_4mins1.mat', 'lfpdata', '-v7.3');
 %%
 clc
 clear
